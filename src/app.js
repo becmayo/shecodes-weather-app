f74686500d4a62e1826637e1d1afd0864e7c0e5b
becmayo <rjv.mayo@gmail.com>
2021-09-24 11:08:24 +0930

    Added fahrenheit/celsius conversion links

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ function userCity(cityInput) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
+  celsiusTemperature = response.data.main.temp;
   let currentTemp = Math.round(response.data.main.temp);
   let mainTempElement = document.querySelector("#main-temp");
   let cityElement = document.querySelector("#city");
@@ -69,9 +69,34 @@ function displayTemperature(response) {
   currentTime();
 }
 
+function celsiusConversion(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let mainTempElement = document.querySelector("#main-temp");
+  mainTempElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function fahrenheitConversion(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let mainTempElement = document.querySelector("#main-temp");
+  mainTempElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
 currentTime();
+
+let celsiusTemperature = null;
 
 navigator.geolocation.getCurrentPosition(showPosition);
 
 let citySearch = document.querySelector("#city-search");
 citySearch.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#celsius-unit");
+celsiusLink.addEventListener("click", celsiusConversion);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-unit");
+fahrenheitLink.addEventListener("click", fahrenheitConversion);
