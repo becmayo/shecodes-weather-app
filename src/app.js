5df9aa4fe148b46b098a25964952a317a9277baf
becmayo <rjv.mayo@gmail.com>
2021-09-28 13:37:10 +0930

    Integrated forecast API to js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,14 @@ function currentTime() {
   dayTime.innerHTML = `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function currentPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
@@ -67,31 +75,45 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = response.data.wind.speed;
   currentTime();
-  displayForecast();
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = "d34bf6dc9d2d08c43bc76d224bf2c78a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tues"];
   let forecastHTML = `<div class = "row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `         <div class="col">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `         <div class="col">
                   <div class="forecast-preview">
-                    <div class="forecast-day">${day}</div>
-                    <img src="icons/01d.png" width="60" height="60" />
+                    <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+                    <img src="icons/${
+                      forecastDay.weather[0].icon
+                    }.png" width="60" height="60" />
                     <div class="forecast-temperature">
-                      <span class="forecast-temperature-max">26°</span> /
-                      <span class="forecast-temperature-min">19°</span>
+                      <span class="forecast-temperature-max">${Math.round(
+                        forecastDay.temp.max
+                      )}</span> /
+                      <span class="forecast-temperature-min">${Math.round(
+                        forecastDay.temp.min
+                      )}</span>
                     </div>
                   </div>
                 </div>
               `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function celsiusConversion(event) {
